dc074f3b684b457c255b578b862506268cba6863
codingwithveer18 <shreekrishna1119@gmail.com>
2023-04-04 17:45:27 +0530

    First commit

---
+++ form.php
@@ -0,0 +1,79 @@
+<?php
+$mongo= new MongoClient();
+$db=$mongo->
+$errorMSG = "NOT FOUND";
+
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+// Mobile NO
+if (empty($_POST["mobile"])) {
+    $errorMSG .= "Number is required ";
+} else {
+    $guest = $_POST["Mobile"];
+}
+
+
+// MSG Event
+if (empty($_POST["event"])) {
+    $errorMSG .= "Subject is required ";
+} else {
+    $event = $_POST["event"];
+}
+
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+
+
+$EmailTo = "gaming.zone24680@gmail.com";
+$Subject = "New Message Received";
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "guest: ";
+$Body .= $guest;
+$Body .= "\n";
+$Body .= "event: ";
+$Body .= $event;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+?>
---
+++ script.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed('.auto-type', {
+    strings: ['a FullStack Developer', 'an Undergrad' , 'Sophomore' , 'Keen Learner'],
+    typeSpeed: 100,
+    backSpeed : 50,
+  });
+  
