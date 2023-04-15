9d1f10970b7fe0350dc3fcfd4dc996bdc46e3ea9
codingwithveer18 <shreekrishna1119@gmail.com>
2023-04-15 18:33:41 +0530

    Changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed('.auto-type', {
-    strings: ['a FullStack Developer', 'an Undergrad' , 'Sophomore' , 'Keen Learner' , 'a GDSC Lead '],
+    strings: ['a FullStack Developer', 'a Dedicated Learner' , 'Sophomore' , 'Keen Learner' , 'a GDSC Lead '],
     typeSpeed: 100,
     backSpeed : 50,
     loop : true,
