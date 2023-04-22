3c49f7c9167ba375a442b0038b1ebd56bc22b091
codingwithveer18 <shreekrishna1119@gmail.com>
2023-04-22 22:53:10 +0530

    first

--- form.php
+++ form.php
@@ -1,58 +1,28 @@
 <?php
-$mongo= new MongoClient();
-$db=$mongo->local;
-$collection=$db->help;
+// Connect to MongoDB
+$mongoClient = new MongoDB\Client("mongodb://localhost:27017");
 
-$errorMSG = "NOT FOUND";
-{
-$insert= array(
-    name=>$_POST["name"],
-    email=>$_POST["email"],
-    mobile=>$_POST["mobile"],
-    Message=>$_POST["Message"],
-);
-if($collection->insert($insert))
-echo "Data is Inserted"
+// Select database and collection
+$database = $mongoClient->formcontent;
+$collection = $database->Documents;
+
+// Check if form is submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    // Create document to insert
+    $document = [
+        'name' => $name,
+        'email' => $email,
+    ];
+
+    // Insert document
+    $result = $collection->insertOne($document);
+
+    // Redirect to thank you page
+    header('Location: thank-you.php');
+    exit;
 }
-else{
-    echo "ERROR"
-}
-else{
-    echo "No data is stored"
-}
-$EmailTo = "gaming.zone24680@gmail.com";
-$Subject = "New Message Received";
-
-// prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "guest: ";
-$Body .= $guest;
-$Body .= "\n";
-$Body .= "event: ";
-$Body .= $event;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
-
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
-
-// redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
-        echo "Something went wrong :(";
-    } else {
-        echo $errorMSG;
-    }
-}
-
 ?>
--- playground-2.mongodb.js
+++ playground-2.mongodb.js
@@ -1,43 +1,60 @@
-/* global use, db */
-// MongoDB Playground
-// To disable this template go to Settings | MongoDB | Use Default Template For Playground.
-// Make sure you are connected to enable completions and to be able to run a playground.
-// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
-// The result of the last command run in a playground is shown on the results panel.
-// By default the first 20 documents will be returned with a cursor.
-// Use 'console.log()' to print to the debug output.
-// For more documentation on playgrounds please refer to
-// https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
+const express = require("express");
+const path = require("path");
 
-// Select the database to use.
-use('mongodbVSCodePlaygroundDB');
+const mongoose = require('mongoose');
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017');
 
-// Insert a few documents into the sales collection.
-db.getCollection('sales').insertMany([
-  { 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
-  { 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
-  { 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
-  { 'item': 'xyz', 'price': 5, 'quantity': 20, 'date': new Date('2014-04-04T11:21:39.736Z') },
-  { 'item': 'abc', 'price': 10, 'quantity': 10, 'date': new Date('2014-04-04T21:23:13.331Z') },
-  { 'item': 'def', 'price': 7.5, 'quantity': 5, 'date': new Date('2015-06-04T05:08:13Z') },
-  { 'item': 'def', 'price': 7.5, 'quantity': 10, 'date': new Date('2015-09-10T08:43:00Z') },
-  { 'item': 'abc', 'price': 10, 'quantity': 5, 'date': new Date('2016-02-06T20:20:13Z') },
-]);
+  // define mongoose schema
+  const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
 
-// Run a find command to view items sold on April 4th, 2014.
-const salesOnApril4th = db.getCollection('sales').find({
-  date: { $gte: new Date('2014-04-04'), $lt: new Date('2014-04-05') }
-}).count();
+  const Contact = mongoose.model('Contact', contactSchema);
+  app.post('/contact', (req, res) => {
+    var mydata = new Contact(req.body);
+    mydata.save().then(() => {
+      res.send("this item has been saved to database")
+    }).catch(() => {
+      res.status(200).render('item was not saved to the database')
+    });
 
-// Print a message to the output window.
-console.log(`${salesOnApril4th} sales occurred in 2014.`);
+    // res.status(200).render('contact.pug');
+  })
 
-// Here we run an aggregation and open a cursor to the results.
-// Use '.toArray()' to exhaust the cursor to return the whole result set.
-// You can use '.hasNext()/.next()' to iterate through the cursor page by page.
-db.getCollection('sales').aggregate([
-  // Find all of the sales that occurred in 2014.
-  { $match: { date: { $gte: new Date('2014-01-01'), $lt: new Date('2015-01-01') } } },
-  // Group the total sales for each product.
-  { $group: { _id: '$item', totalSaleAmount: { $sum: { $multiply: [ '$price', '$quantity' ] } } } }
-]);
+}
+const bodyparser = require("body-parser")
+
+
+const app = express();
+const port = 8000;
+
+
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+// ENDPOINTS
+app.get('/', (req, res) => {
+  const params = {}
+  res.status(200).render('index.html', params);
+})
+app.get('/contact', (req, res) => {
+  const params = {}
+  res.status(200).render('index.html', params)
+})
+
+// START THE SERVER
+app.listen(port, () => {
+  console.log(`The application started successfully on port ${port}`);
+});
